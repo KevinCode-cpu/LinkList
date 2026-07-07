0ba43aae804095f9e400fa117bd41d74c9aeafc4
Satyam Verma <satyamvr33@gmail.com>
2026-07-07 14:12:48 +0530

    credential

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -2,22 +2,18 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
-print("Current file:", __file__)
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print("BASE_DIR:", BASE_DIR)
-
-cred_path = os.path.join(
-    BASE_DIR,
-    "..",
-    "credentials",
-    "firebase-admin.json"
+# On Render use secret file, locally use credentials folder
+cred_path = os.getenv(
+    "FIREBASE_CREDENTIALS",
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "credentials",
+            "firebase-admin.json"
+        )
+    )
 )
-
-cred_path = os.path.abspath(cred_path)
-
-print("Credential path:", cred_path)
-print("Exists:", os.path.exists(cred_path))
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(cred_path)
